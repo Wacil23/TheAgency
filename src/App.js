b603e3ca64d1d34487c769c0ef01da0a0dc51ed4
wacil23 <wacilzek00@gmail.com>
2022-10-21 00:55:39 +0200

    Header finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import JumpBoy from './assets/img/with shadow/Digital/he_in_jump.png'
+import JumpGirl from './assets/img/with shadow/Digital/she_in_jump.png'
+import Navbar from './components/Navbar';
+import Parallax from 'parallax-js'
+import { useEffect, useRef } from 'react';
 
-function App() {
+const App = () => {
+
+  const sceneEl = useRef(null);
+  useEffect(() => {
+    const parallaxInstance = new Parallax(sceneEl.current)
+
+    parallaxInstance.enable();
+
+    return () => parallaxInstance.disable();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='bg-[#8a47ff]' >
+        <Navbar />
+        <div className="content flex justify-center content-center items-center">
+          <div className="flex justify-center items-center content-center w-full h-[90vh]">
+            <h1 className={`text-9xl font-gazpacho text-center  absolute text-[#ffca2b] uppercase`}>the agency.</h1>
+            <h1 className='text-9xl font-gazpacho text-center   absolute text-[#ffca2b] uppercase'>the agency.</h1>
+            <h1 className='text-9xl font-gazpacho text-center absolute text-[#ffca2b] uppercase'>the agency.</h1>
+            <h1 className='text-9xl font-gazpacho text-center   absolute text-[#ffca2b] uppercase'>the agency.</h1>
+            <h1 className='text-9xl font-gazpacho text-center  absolute text-[#ffca2b] uppercase'>the agency.</h1>
+            <h1 className='text-9xl font-gazpacho text-center  absolute text-[#ffca2b] uppercase'>the agency.</h1>
+            <div ref={sceneEl} >
+              <img className='w-[50%] absolute boy' data-depth='0.2' src={JumpBoy} alt="" style={{position: 'sticky'}}/>
+              <img className='w-[50%] absolute girl' data-depth='0.2' src={JumpGirl} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-100 top-0 sticky'>
+        <div className=' h-20 flex justify-around items-center'>
+            <span className='text-[#ffca2b] font-bold uppercase'> <span className='font-thin'>The</span> Agency</span>
+            <ul className='flex'>
+                <li className='ml-8'>Accueil</li>
+                <li className='ml-8'>Accueil</li>
+                <li className='ml-8'>Accueil</li>
+                <li className='ml-8'>Accueil</li>
+                <li className='ml-8'>Accueil</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
