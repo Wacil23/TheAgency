282fa0bc807ad60ed74ecb1eac812b166f3b3ae2
wacil23 <wacilzek00@gmail.com>
2022-10-21 19:12:41 +0200

    Navbar with scrollTrigger gsap set

--- src/App.js
+++ src/App.js
@@ -4,37 +4,59 @@ import JumpGirl from './assets/img/with shadow/Digital/she_in_jump.png'
 import Navbar from './components/Navbar';
 import Parallax from 'parallax-js'
 import { useEffect, useRef } from 'react';
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 const App = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  const parallaxRef = useRef(null)
+  const h1Ref = useRef(null)
+  useEffect(() => {
+    const parallax = parallaxRef.current
+    const h1 = h1Ref.current
+
+    gsap.to(h1, {
+      scrollTrigger: {
+        trigger: parallax,
+        markers: false,
+        start: 'top top',
+        end: 'bottom bottom',
+        scrub: true,
+        toggleActions: 'restart pause reverse none'
+      },
+      opacity: '0',
+      scale: 0
+    })
+  })
 
   const sceneEl = useRef(null);
+
   useEffect(() => {
     const parallaxInstance = new Parallax(sceneEl.current)
-
     parallaxInstance.enable();
-
     return () => parallaxInstance.disable();
   }, [])
 
   return (
     <>
-      <div className='bg-[#8a47ff]' >
+      <div className='bg-[#8a47ff]' ref={parallaxRef}>
         <Navbar />
         <div className="content flex justify-center content-center items-center">
           <div className="flex justify-center items-center content-center w-full h-[90vh]">
-            <h1 className={`text-9xl font-gazpacho text-center  absolute text-[#ffca2b] uppercase`}>the agency.</h1>
-            <h1 className='text-9xl font-gazpacho text-center   absolute text-[#ffca2b] uppercase'>the agency.</h1>
-            <h1 className='text-9xl font-gazpacho text-center absolute text-[#ffca2b] uppercase'>the agency.</h1>
-            <h1 className='text-9xl font-gazpacho text-center   absolute text-[#ffca2b] uppercase'>the agency.</h1>
-            <h1 className='text-9xl font-gazpacho text-center  absolute text-[#ffca2b] uppercase'>the agency.</h1>
-            <h1 className='text-9xl font-gazpacho text-center  absolute text-[#ffca2b] uppercase'>the agency.</h1>
-            <div ref={sceneEl} >
-              <img className='w-[50%] absolute boy' data-depth='0.2' src={JumpBoy} alt="" style={{position: 'sticky'}}/>
-              <img className='w-[50%] absolute girl' data-depth='0.2' src={JumpGirl} alt="" />
+            <h1 className={`text-9xl font-gazpacho text-center  absolute text-[#ffca2b] uppercase`} data-depth='0.05'>the agency.</h1>
+            <h1 className='text-9xl font-gazpacho text-center   absolute text-[#ffca2b] uppercase' data-depth='0.05'>the agency.</h1>
+            <h1 className='text-9xl font-gazpacho text-center absolute text-[#ffca2b] uppercase' data-depth='0.05'>the agency.</h1>
+            <h1 className='text-9xl font-gazpacho text-center   absolute text-[#ffca2b] uppercase' data-depth='0.05'>the agency.</h1>
+            <h1 className='text-9xl font-gazpacho text-center  absolute text-[#ffca2b] uppercase' data-depth='0.05'>the agency.</h1>
+            <h1 className='text-9xl font-gazpacho text-center  absolute text-[#ffca2b] uppercase' data-depth='0.05'>the agency.</h1>
+            <div ref={sceneEl} className='fixed'>
+              <img className='w-[50%]  boy' data-depth='0.05' src={JumpBoy} alt="" ref={h1Ref}/>
+              <img className='w-[50%] absolute girl' data-depth='0.07' src={JumpGirl} alt="" ref={h1Ref}/>
             </div>
           </div>
         </div>
       </div>
+      <div className="bg-[#8a47ff] absolute w-screen h-screen"></div>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,57 @@
 import React from 'react'
+import { useEffect, useRef } from 'react';
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 const Navbar = () => {
+
+  gsap.registerPlugin(ScrollTrigger);
+  const link1 = useRef(null)
+  const link2 = useRef(null)
+  const link3 = useRef(null)
+  const link4 = useRef(null)
+  const link5 = useRef(null)
+  const container = useRef(null)
+
+  useEffect(() => {
+    const cont = container.current
+    const link1Ref = link1.current
+    const link2Ref = link2.current
+    const link3Ref = link3.current
+    const link4Ref = link4.current
+    const link5Ref = link5.current
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: cont,
+        start: 'top top',
+        end: '+=20',
+        snap: 1,
+        toggleActions: 'play none reverse reverse',
+      },
+    })
+
+    tl.to(link1Ref, { translateY: '-100px', duration: 0.3 })
+      .to(link2Ref, { translateY: '-100px', duration: 0.3 })
+      .to(link3Ref, { translateY: '-100px', duration: 0.3 })
+      .to(link4Ref, { translateY: '-100px', duration: 0.3 })
+      .to(link5Ref, { translateY: '-100px', duration: 0.3 })
+    return () => ScrollTrigger.killAll()
+  }, [])
+
   return (
-    <div className='w-100 top-0 sticky'>
-        <div className=' h-20 flex justify-around items-center'>
-            <span className='text-[#ffca2b] font-bold uppercase'> <span className='font-thin'>The</span> Agency</span>
-            <ul className='flex'>
-                <li className='ml-8'>Accueil</li>
-                <li className='ml-8'>Accueil</li>
-                <li className='ml-8'>Accueil</li>
-                <li className='ml-8'>Accueil</li>
-                <li className='ml-8'>Accueil</li>
-            </ul>
-        </div>
+    <div className='w-100 top-0 sticky z-50' >
+      <div className=' h-20 flex justify-around items-center' ref={container} >
+        <span className='text-[#ffca2b] font-bold uppercase'>
+          <span className='font-thin'>The</span> Agency</span>
+        <ul className='flex text-white'  >
+          <li className='ml-8' ref={link1}>Accueil</li>
+          <li className='ml-8' ref={link2}>Digital Marketing</li>
+          <li className='ml-8' ref={link3}>Notre missions</li>
+          <li className='ml-8' ref={link4}>Accueil</li>
+          <li className='ml-8' ref={link5}>Accueil</li>
+        </ul>
+      </div>
     </div>
   )
 }
