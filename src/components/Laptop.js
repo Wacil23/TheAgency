f427faf771db8a3bbc642e0313e906ba7ba9d818
wacil23 <wacilzek00@gmail.com>
2023-01-03 10:11:16 +0100

    Custom module and change groups and meshes by primitive

--- src/components/Laptop.js
+++ src/components/Laptop.js
@@ -1,106 +1,113 @@
 import React from 'react'
 import {
-    Center,
-    ContactShadows,
     Environment,
-    Float,
     Html,
     PresentationControls,
     useGLTF,
     useScroll,
 
 } from '@react-three/drei';
-import gsap from 'gsap'
-import ScrollTrigger from 'gsap/ScrollTrigger';
-import { useFrame, useThree } from '@react-three/fiber';
-import useRefs from 'react-use-refs'
-import * as THREE from 'three'
-import { Camera } from 'three';
+import { useFrame } from '@react-three/fiber';
 
-
-const Laptop = (props) => {
+const Laptop = ({ portal, getPositionScrollFromChild, ...props }) => {
+    // const { nodes, materials } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf');
+    // const { width, height } = useThree((state) => state.viewport)
+    // const [group, mac, site] = useRefs()
+    // gsap.registerPlugin(ScrollTrigger);
+    // const domContent = useRef(null);
     const scroll = useScroll();
-    const { width, height } = useThree((state) => state.viewport)
-    const [group, mac, site] = useRefs()
-    gsap.registerPlugin(ScrollTrigger);
     const rsqw = (t, delta = 0.1, a = 1, f = 1 / (2 * Math.PI)) => (a / Math.atan(1 / delta)) * Math.atan(Math.sin(2 * Math.PI * t * f) / delta)
 
+    const { scene, nodes } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
 
-    const { nodes, materials } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf');
     useFrame((state, delta) => {
+        getPositionScrollFromChild(window.scrollY)
         const r1 = scroll.range(0 / 4, 1 / 4)
         const r2 = scroll.range(2 / 4, 2 / 4)
-        const r3 = scroll.visible(4 / 5, 1 / 5)
-        mac.current.rotation.x = Math.PI - (Math.PI / 2) * rsqw(r1) + r2 / 12
-        group.current.scale.x = group.current.scale.y = group.current.scale.z = THREE.MathUtils.damp(group.current.scale.z, 1 + 4 * (0.1 + rsqw(r2)), 4, delta);
-        group.current.position.y = THREE.MathUtils.damp(group.current.position.y, 1 - 0.09 * (11 + rsqw(r1)), 4, delta);
-        group.current.rotation.x = THREE.MathUtils.damp(group.current.rotation.x, 2 - 0.3 * (6.5 + rsqw(r2)), 4, delta)
-        site.current.classList.toggle("show", r3)
+        nodes.Top.rotation.x = Math.PI - (Math.PI / 2) * rsqw(r1) + r2 / 12
+        //   const r3 = scroll.visible(4 / 5, 1 / 5)
+        //    mac.current.rotation.x = Math.PI - (Math.PI / 2) * rsqw(r1) + r2 / 12
+        //     group.current.scale.x = group.current.scale.y = group.current.scale.z = THREE.MathUtils.damp(group.current.scale.z, 1 + 4 * (0.1 + rsqw(r2)), 4, delta);
+        //    group.current.position.y = THREE.MathUtils.damp(group.current.position.y, 1 - 0.09 * (11 + rsqw(r1)), 4, delta);
+        //    group.current.rotation.x = THREE.MathUtils.damp(group.current.rotation.x, 2 - 0.3 * (6.5 + rsqw(r2)), 4, delta)
     })
-
     return (
 
-        <group>
+        <>
             <Environment preset='city' />
-            <PresentationControls
-                polar={[-0.2, 0.03]}
-                azimuth={[-1, 0.75]}
-                config={{ mass: 2, tension: 400 }}
-            >
-
-                <group ref={group} {...props} dispose={null}>
-                    <group position={[0, -height / 12, 0,]} scale={width / 180} >
-
-
-                        <mesh geometry={nodes.Circle001.geometry} material={nodes.Circle001.material} />
-                        <mesh geometry={nodes.Circle001_1.geometry} material={nodes.Circle001_1.material} />
-                        <mesh geometry={nodes.Circle001_2.geometry} material={materials.HeadPhoneHole} />
-                        <mesh geometry={nodes.Circle001_3.geometry} material={nodes.Circle001_3.material} />
-                        <mesh geometry={nodes.Circle001_4.geometry} material={nodes.Circle001_4.material} />
-                        <mesh geometry={nodes.Circle001_5.geometry} material={materials.TouchbarBorder} />
-                        <mesh geometry={nodes.Circle001_6.geometry} material={materials.Keyboard} />
-                        <mesh geometry={nodes.KeyboardKeyHole.geometry} material={nodes.KeyboardKeyHole.material} position={[-11.79, -0.15, -8.3,]} scale={5.8} />
-                        <mesh geometry={nodes.RubberFoot.geometry} material={materials.DarkRubber} position={[-11.95, -0.75, 7.86,]} scale={5.8} />
-                        <group position={[0.01, -0.21, -10.56,]} scale={5.8} >
-                            <mesh geometry={nodes.Circle012.geometry} material={materials.HingeBlack} />
-                            <mesh geometry={nodes.Circle012_1.geometry} material={materials.HingeMetal} />
-                        </group>
-                        <group position={[0, -0.51, 0,]} scale={5.8} >
-                            <mesh geometry={nodes.Circle006.geometry} material={nodes.Circle006.material} />
-                            <mesh geometry={nodes.Circle006_1.geometry} material={nodes.Circle006_1.material} />
-                        </group>
-                        <group position={[-11.79, -0.15, -8.3,]} scale={5.8} >
-                            <mesh geometry={nodes.Circle.geometry} material={nodes.Circle.material} />
-                            <mesh geometry={nodes.Circle_1.geometry} material={materials.Key} />
-                            <mesh geometry={nodes.Circle_2.geometry} material={materials.Touchbar} />
-                        </group>
-                        <group ref={mac} position={[0.01, -0.47, -10.41,]} scale={[5.8, 7, 5.71]} >
-                            <mesh geometry={nodes.Circle002.geometry} material={nodes.Circle002.material} />
-                            <mesh geometry={nodes.Circle002_1.geometry} material={materials.Screen} />
-                            <mesh geometry={nodes.Circle002_2.geometry} material={materials.ScreenGlass} />
-                            <mesh geometry={nodes.Circle002_3.geometry} material={materials.Rubber} />
-                            <mesh geometry={nodes.Circle002_4.geometry} material={materials.DisplayGlass} />
-                            <mesh geometry={nodes.AppleLogo000.geometry} material={materials['AppleLogo.004']} position={[0, -0.11, -1.8,]} rotation={[-Math.PI, 0, -Math.PI,]} scale={[0.58, 0.58, 0.58,]} />
-                        </group>
-                        <group position={[12.2, 0.03, 0.6,]} scale={5.8} >
-                            <mesh geometry={nodes.Circle003.geometry} material={nodes.Circle003.material} />
-                            <mesh geometry={nodes.Circle003_1.geometry} material={nodes.Circle003_1.material} />
-                        </group>
-                        <group position={[-15.03, 0.03, 0.6,]} scale={5.8} >
-                            <mesh geometry={nodes.Circle009.geometry} material={nodes.Circle009.material} />
-                            <mesh geometry={nodes.Circle009_1.geometry} material={nodes.Circle009_1.material} />
-                        </group>
-
-                    </group>
-                </group>
-                <Html ref={site} center >
-                    <h1>serhqjskdhkj</h1>
-                </Html>
+            <PresentationControls polar={[-0.2, 0.03]} azimuth={[-1, 0.75]} config={{ mass: 2, tension: 400 }} >
+                <primitive position-y={-2} object={scene} {...props}>
+                    <Html transform wrapperClass='frame' position={[0, 1.56, -1.4]} distanceFactor={1.17} >
+                        <iframe className='' src="https://yummy-wok.fr" title='web'>
+                            <h1>Hello dfsdfsdf</h1>
+                        </iframe>
+                    </Html>
+                </primitive>
             </PresentationControls>
-            <ContactShadows position-y={-height / 4} opacity={0.4} blur={2.4} scale={9} />
-        </group>
+        </>
+
+        // <group ref={domContent}>
+        //     <Environment preset='city' />
+        //     <PresentationControls
+        //         polar={[-0.2, 0.03]}
+        //         azimuth={[-1, 0.75]}
+        //         config={{ mass: 2, tension: 400 }}
+        //     >
+        //         <group ref={group} {...props} dispose={null}>
+        //             <group position={[0, -height / 12, 0,]} scale={width / 180} >
+        //                 <mesh geometry={nodes.Circle001.geometry} material={nodes.Circle001.material} />
+        //                 <mesh geometry={nodes.Circle001_1.geometry} material={nodes.Circle001_1.material} />
+        //                 <mesh geometry={nodes.Circle001_2.geometry} material={materials.HeadPhoneHole} />
+        //                 <mesh geometry={nodes.Circle001_3.geometry} material={nodes.Circle001_3.material} />
+        //                 <mesh geometry={nodes.Circle001_4.geometry} material={nodes.Circle001_4.material} />
+        //                 <mesh geometry={nodes.Circle001_5.geometry} material={materials.TouchbarBorder} />
+        //                 <mesh geometry={nodes.Circle001_6.geometry} material={materials.Keyboard} />
+        //                 <mesh geometry={nodes.KeyboardKeyHole.geometry} material={nodes.KeyboardKeyHole.material} position={[-11.79, -0.15, -8.3,]} scale={5.8} />
+        //                 <mesh geometry={nodes.RubberFoot.geometry} material={materials.DarkRubber} position={[-11.95, -0.75, 7.86,]} scale={5.8} />
+        //                 <group position={[0.01, -0.21, -10.56,]} scale={5.8} >
+        //                     <mesh geometry={nodes.Circle012.geometry} material={materials.HingeBlack} />
+        //                     <mesh geometry={nodes.Circle012_1.geometry} material={materials.HingeMetal} />
+        //                 </group>
+        //                 <group position={[0, -0.51, 0,]} scale={5.8} >
+        //                     <mesh geometry={nodes.Circle006.geometry} material={nodes.Circle006.material} />
+        //                     <mesh geometry={nodes.Circle006_1.geometry} material={nodes.Circle006_1.material} />
+        //                 </group>
+        //                 <group position={[-11.79, -0.15, -8.3,]} scale={5.8} >
+        //                     <mesh geometry={nodes.Circle.geometry} material={nodes.Circle.material} />
+        //                     <mesh geometry={nodes.Circle_1.geometry} material={materials.Key} />
+        //                     <mesh geometry={nodes.Circle_2.geometry} material={materials.Touchbar} />
+        //                 </group>
+        //                 <group ref={mac} position={[0.01, -0.47, -10.41,]} scale={[5.8, 7, 5.71]} >
+        //                     <mesh geometry={nodes.Circle002.geometry} material={nodes.Circle002.material} />
+        //                     <mesh geometry={nodes.Circle002_1.geometry} material={materials.Screen} />
+        //                     <mesh geometry={nodes.Circle002_2.geometry} material={materials.ScreenGlass} />
+        //                     <mesh geometry={nodes.Circle002_3.geometry} material={materials.Rubber} />
+        //                     <mesh geometry={nodes.Circle002_4.geometry} material={materials.DisplayGlass} />
+        //                 </group>
+        //                 <group position={[12.2, 0.03, 0.6,]} scale={5.8} >
+        //                     <mesh geometry={nodes.Circle003.geometry} material={nodes.Circle003.material} />
+        //                     <mesh geometry={nodes.Circle003_1.geometry} material={nodes.Circle003_1.material} />
+        //                 </group>
+        //                 <group position={[-15.03, 0.03, 0.6,]} scale={5.8} >
+        //                     <mesh geometry={nodes.Circle009.geometry} material={nodes.Circle009.material} />
+        //                     <mesh geometry={nodes.Circle009_1.geometry} material={nodes.Circle009_1.material} />
+        //                 </group>
+
+        //                     <mesh geometry={nodes.AppleLogo000.geometry} material={materials['AppleLogo.004']} position={[0, -0.11, -1.8,]} rotation={[-Math.PI, 0, -Math.PI,]} scale={[0.58, 0.58, 0.58,]} />
+        //         <Html position-y={-3}  portal={portal} >
+        //             <iframe className='' src="https://yummy-wok.fr" title='web'>
+        //                 <h1>Hello dfsdfsdf</h1>
+        //             </iframe>
+        //         </Html>
+        //             </group>
+        //         </group>
+        //     </PresentationControls>
+        //     <ContactShadows position-y={-height / 4} opacity={0.4} blur={2.4} scale={9} />
+        // </group>
+
     )
 }
+
 
 useGLTF.preload('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf');
 export default Laptop
--- src/pages/SiteWeb.js
+++ src/pages/SiteWeb.js
@@ -1,6 +1,6 @@
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import Parallax from 'parallax-js';
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import { BsChevronDoubleDown } from 'react-icons/bs'
 import gsap from 'gsap'
 import missionAvatar from '../assets/img/with shadow/Digital/he_multitasking.png'
@@ -12,7 +12,13 @@ const SiteWeb = () => {
     gsap.registerPlugin(ScrollTrigger);
     const container = useRef(null)
     const sceneEl = useRef(null);
-    
+    const domContent = useRef(null);
+    const [currentHeightScroll, setCurrentHeightScroll] = useState(0);
+
+    const getPositionScrollFromChild = (scrollY) => {
+        setCurrentHeightScroll(scrollY)
+    }
+
     useEffect(() => {
         const cont = container.current
         const ctx = gsap.context(() => {
@@ -21,42 +27,49 @@ const SiteWeb = () => {
                     trigger: cont,
                     start: '25px top',
                     end: '+=20',
-                    snap: 5 ,
+                    snap: 5,
                     toggleActions: 'restart none reverse reverse',
                 },
             })
-            .to(cont, { opacity: '0' })
+                .to(cont, { opacity: '0' })
         }, container)
-
         return () => ctx.revert()
     }, [])
+
+    useLayoutEffect(() => {
+        if ((window.innerHeight - currentHeightScroll) < 0.5) {
+            console.log('you are in bottom')
+        }
+    })
+
     useEffect(() => {
         const parallaxInstance = new Parallax(sceneEl.current)
         parallaxInstance.enable();
         return () => parallaxInstance.disable();
     }, [])
+
     return (
-        <>
+        <div>
             <div className=' bg-gradient-to-t from-[#ff5352] to-[#e14b4a] w-full h-screen'>
                 <div className='content flex-col flex justify-center content-center items-center w-full h-full' ref={container}>
                     <h1 className={`text-[15rem] text-[#1B065E] font-mabryBlack text-center uppercase`} >Site Web</h1>
                     <div ref={sceneEl} className='fixed w-full left-[40%]'>
-                        {/* <img className='absolute left-[40%] w-[36%] ' src={missionAvatar} alt="" data-depth='0.05' /> */}
+                        <img className='absolute left-[40%] w-[36%] ' src={missionAvatar} alt="" data-depth='0.05' />
                     </div>
                     <div className='flex justify-center'>
                         <span className={`absolute bottom-[10%] font-mabryLight text-2xl text-white`}>" Un site sans marketing ? Ça existe encore ?? "</span>
                         <span className='absolute flex items-center text-white bottom-0 mb-10 mx-auto animate-pulse'>Scroll down <BsChevronDoubleDown className='ml-2' /></span>
                     </div>
                 </div>
-                <div className='w-full h-screen bg-gradient-to-t from-[#ea5d5d] to-[#ff5352]'>
-                    <Canvas dpr={[1, 2]} flat shadows >
-                        <ScrollControls pages={5}>
-                            <Laptop />
-                        </ScrollControls>
+                <div ref={domContent} className='w-full h-screen bg-gradient-to-t from-[#ea5d5d] to-[#ff5352]'>
+                    <Canvas dpr={[1, 2]} camera={{ fov: 65 }} flat shadows >
+                            <ScrollControls key={2} pages={3}>
+                                <Laptop portal={domContent} getPositionScrollFromChild={getPositionScrollFromChild} />
+                            </ScrollControls>
                     </Canvas>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
